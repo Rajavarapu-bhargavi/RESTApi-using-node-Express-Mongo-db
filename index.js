3888f91761ba81552ded6222ebfa8f8f5c48426a
Rajavarapu-bhargavi <144330922+Rajavarapu-bhargavi@users.noreply.github.com>
2024-11-30 11:31:20 +0530

    index.js

--- index.js
+++ index.js
@@ -17,29 +17,3 @@ app.use('/api',router);
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`)
 })
-
-
-// Load environment variables
-// require('dotenv').config();
-
-// // Import necessary modules
-// const mongoose = require('mongoose');
-// const express = require('express');
-// const app = express();
-
-// // MongoDB Atlas connection
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB Atlas'))
-//   .catch(err => console.error('MongoDB Atlas connection error:', err));
-
-// // Middleware to parse JSON
-// app.use(express.json());
-
-// // Import routes and use them
-// const router = require('./routes/routes');
-// app.use('/api', router);
-
-// // Start the server
-// app.listen(3000, () => {
-//   console.log(`Server Started at http://localhost:3000`);
-// });
